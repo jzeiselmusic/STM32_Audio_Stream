d513a0e8cb2603be7c7ea70f7fecacbf51fbd6b6
Jake Zeisel <jacobzeisel@gmail.com>
2023-07-02 14:15:41 -0500

    add CMakeLists.txt. add download toolchain sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# setup environment
+
+# download toolchain
+rm -rf out.tar.xz
+curl -L https://developer.arm.com/-/media/Files/downloads/gnu/12.2.mpacbti-rel1/binrel/arm-gnu-toolchain-12.2.mpacbti-rel1-darwin-arm64-arm-none-eabi.tar.xz --output out.tar.xz
+
+# unzip to new folder
+
+# make folder if it doesnt exist
+mkdir -p ./arm_toolchain
+rm -rf ./arm_toolchain/*
+echo "\nextracting toolchain...\n"
+tar -xf out.tar.xz --directory=./arm_toolchain
+
